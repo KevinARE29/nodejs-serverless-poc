b4cb24f0381b723ccfca9ec21b710a15616b1385
Kevin Rodríguez <kevinare295@gmail.com>
2021-08-23 18:27:54 -0600

    test: add e2e tests

--- infrastructure/test/serverless-api-nest.test.ts
+++ infrastructure/test/serverless-api-nest.test.ts
@@ -1,18 +1,9 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
 import { APIStack } from '../lib'
 
-test('Empty Stack', () => {
+test('API Stack', () => {
   const app = new cdk.App()
-  // WHEN
-  const stack = new APIStack(app, 'MyTestStack')
-  // THEN
-  expectCDK(stack).to(
-    matchTemplate(
-      {
-        Resources: {},
-      },
-      MatchStyle.EXACT,
-    ),
-  )
+  const stack = new APIStack(app, 'APIStack')
+
+  expect(stack).toBeDefined()
 })
