4061a52eb69c6998604f6977764eda81aafe9aec
Kevin Escoto <kevinescoto@ravn.co>
2021-09-03 11:49:41 -0600

    refactor: remove nestjs layer

--- api/movies/src/dtos/request/create-attachment.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ContentTypeEnum, FileExtensionEnum } from '../../enums'
-
-export class CreateAttachmentDto {
-  readonly uuid: string
-  readonly contentType: ContentTypeEnum
-  readonly ext: FileExtensionEnum
-  readonly filename: string
-}
--- api/movies/src/dtos/request/update-movie.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger'
-import { CreateMovieDto } from './create-movie.dto'
-
-export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
